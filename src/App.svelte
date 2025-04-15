fae157d297d79c3f2abe2c8b8bab756c9c9466c0
Alex Ledger <alex@playgroundlabs.io>
2025-04-15 09:52:57 -0700

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { theme, initializeTheme } from "./lib/stores/theme";
-
-  import ThemeToggle from "./lib/components/ThemeToggle.svelte";
-  import Sidebar from "./lib/components/Sidebar.svelte";
   import MainContent from "./lib/components/MainContent.svelte";
 
   onMount(() => {
@@ -11,14 +8,8 @@
   });
 </script>
 
-<div class="h-screen bg-base-100" data-theme={$theme}>
-  <div class="flex h-full">
-    <Sidebar />
-    <main class="flex-1 p-4">
-      <div class="flex justify-end mb-4">
-        <ThemeToggle />
-      </div>
-      <MainContent />
-    </main>
-  </div>
+<div class="h-screen bg-base-200" data-theme={$theme}>
+  <main class="pt-8 px-4">
+    <MainContent />
+  </main>
 </div>
--- src/lib/components/MainContent.svelte
+++ src/lib/components/MainContent.svelte
@@ -49,7 +49,7 @@
     }
   }
 
-  async function fetchPlayerHistory(playerId: string) {
+  async function fetchPlayerHistory(playerId: number) {
     const url = `https://api.dupr.gg/player/v1.0/${playerId}/history?limit=25&offset=0`;
     const response = await fetch(url, {
       headers: {
@@ -72,8 +72,11 @@
 
 <div class="flow">
   <h1 class="text-2xl font-bold">PB Insight</h1>
-  <div class="card bg-base-200 shadow-xl">
-    <div class="card-body">
+
+  <!-- Token Input -->
+  <details class="collapse collapse-arrow bg-base-100 border-base-300 border">
+    <summary class="collapse-title font-semibold">DUPR Token Input</summary>
+    <div class="collapse-content">
       <div class="form-control">
         <label class="label" for="token">Token</label>
         <input
@@ -85,13 +88,15 @@
         />
       </div>
       <button class="btn btn-primary w-fit mt-4" onclick={saveToken}
-        >Save Token (might be insecure)</button
+        >Store token</button
       >
     </div>
-  </div>
+  </details>
 
-  <div class="card bg-base-200 shadow-xl">
+  <!-- Search Form -->
+  <div class="card bg-base-100 border-base-300">
     <div class="card-body">
+      <h2 class="card-title">Search for a player</h2>
       <form class="flow" onsubmit={searchUser}>
         <div class="form-control">
           <label class="label" for="username">Username</label>
@@ -108,88 +113,93 @@
   </div>
 
   {#if player}
-    <div class="card bg-base-200 shadow-xl">
+    <div class="card bg-base-100 border-base-300">
       <div class="card-body">
         <h2 class="card-title">{player.fullName}</h2>
         <p>Location: {player.shortAddress}</p>
         <p>Doubles DUPR: {player.ratings.doubles}</p>
+
+        {#if playerHistory}
+          <div class="overflow-x-auto">
+            <table class="table table-zebra">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Event</th>
+                  <th>Venue</th>
+                  <th class="min-w-[25ch]">Opponent</th>
+                  <th class="min-w-[25ch]">Partner</th>
+                  <th>Score</th>
+                  <th>Result</th>
+                </tr>
+              </thead>
+              <tbody>
+                {#each playerHistory as match}
+                  {#if match.teams && match.teams.length > 0}
+                    {@const playerTeam = match.teams.find(
+                      (team: ApiMatchTeam) =>
+                        team.player1.id === player?.id ||
+                        team.player2.id === player?.id,
+                    )}
+                    {@const opponentTeam = match.teams.find(
+                      (team: ApiMatchTeam) => team !== playerTeam,
+                    )}
+                    <tr>
+                      <td>{new Date(match.eventDate).toLocaleDateString()}</td>
+                      <td class="truncate">{match.eventName.slice(0, 20)}</td>
+                      <td>{match.venue}</td>
+                      <td>
+                        {#if opponentTeam}
+                          {opponentTeam.player1.fullName} ({opponentTeam.player1
+                            .postMatchRating.doubles})
+                          <br />
+                          {opponentTeam.player2.fullName} ({opponentTeam.player2
+                            .postMatchRating.doubles})
+                        {/if}
+                      </td>
+                      <td>
+                        {#if playerTeam}
+                          {playerTeam.player1.id === player?.id
+                            ? playerTeam.player2.fullName
+                            : playerTeam.player1.fullName}
+                          ({playerTeam.player1.id === player?.id
+                            ? playerTeam.player2.postMatchRating.doubles
+                            : playerTeam.player1.postMatchRating.doubles})
+                        {/if}
+                      </td>
+                      <td>
+                        {#if playerTeam}
+                          {playerTeam.game1}-{opponentTeam?.game1}
+                          {#if playerTeam.game2 !== undefined && playerTeam.game2 !== -1}
+                            , {playerTeam.game2}-{opponentTeam?.game2}
+                          {/if}
+                          {#if playerTeam.game3 !== undefined && playerTeam.game3 !== -1}
+                            , {playerTeam.game3}-{opponentTeam?.game3}
+                          {/if}
+                        {/if}
+                      </td>
+                      <td>
+                        {#if playerTeam}
+                          <span
+                            class="badge {playerTeam.winner
+                              ? 'badge-success'
+                              : 'badge-error'}"
+                          >
+                            {playerTeam.winner ? "W" : "L"}
+                          </span>
+                        {/if}
+                      </td>
+                    </tr>
+                  {/if}
+                {/each}
+              </tbody>
+            </table>
+          </div>
+        {/if}
       </div>
     </div>
   {/if}
 </div>
-
-{#if playerHistory}
-  <div class="card bg-base-200 shadow-xl">
-    <div class="card-body">
-      <h2 class="card-title">Player History</h2>
-      <div class="overflow-x-auto">
-        <table class="table table-zebra">
-          <thead>
-            <tr>
-              <th>Event</th>
-              <th>Date</th>
-              <th>Format</th>
-              <th>Venue</th>
-              <th>Opponent</th>
-              <th>Score</th>
-              <th>Result</th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each playerHistory as match}
-              {#if match.teams && match.teams.length > 0}
-                {@const playerTeam = match.teams.find(
-                  (team: ApiMatchTeam) =>
-                    team.player1.id === player?.id ||
-                    team.player2.id === player?.id,
-                )}
-                {@const opponentTeam = match.teams.find(
-                  (team: ApiMatchTeam) => team !== playerTeam,
-                )}
-                <tr>
-                  <td>{match.eventName}</td>
-                  <td>{new Date(match.eventDate).toLocaleDateString()}</td>
-                  <td>{match.eventFormat}</td>
-                  <td>{match.venue}</td>
-                  <td>
-                    {#if opponentTeam}
-                      {opponentTeam.player1.fullName}
-                      {#if opponentTeam.player2}
-                        / {opponentTeam.player2.fullName}
-                      {/if}
-                    {/if}
-                  </td>
-                  <td>
-                    {#if playerTeam}
-                      {playerTeam.game1}-{opponentTeam?.game1}
-                      {#if playerTeam.game2 !== undefined}
-                        , {playerTeam.game2}-{opponentTeam?.game2}
-                      {/if}
-                      {#if playerTeam.game3 !== undefined}
-                        , {playerTeam.game3}-{opponentTeam?.game3}
-                      {/if}
-                    {/if}
-                  </td>
-                  <td>
-                    {#if playerTeam}
-                      <span
-                        class="badge {playerTeam.winner
-                          ? 'badge-success'
-                          : 'badge-error'}"
-                      >
-                        {playerTeam.winner ? "W" : "L"}
-                      </span>
-                    {/if}
-                  </td>
-                </tr>
-              {/if}
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
-{/if}
 
 <style>
   .flow > * + * {
